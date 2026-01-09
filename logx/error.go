d0ffa80e17fd27fae4dd5f1224da7fe41a0ed456
João Freitas <1160907@isep.ipp.pt>
2026-01-09 20:35:15 +0000

    [logx] - remove extra param

--- logx/error.go
+++ logx/error.go
@@ -15,7 +15,7 @@ func Errorx(err error, extras map[string]any) {
 
 // Error logs a program error.
 // Same as [Error] but includes the error in structured context with key "error".
-func Errore(msg string, err error) {
+func Errore(err error) {
 	for _, l := range loggers {
 		l.Error(err, map[string]any{"error": err})
 	}
