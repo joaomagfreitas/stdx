102ddcc7169ad2074cde5930d02d455b9e6fe60d
João Freitas <1160907@isep.ipp.pt>
2026-01-09 20:35:39 +0000

    [logx] - revert info docs

--- logx/info.go
+++ logx/info.go
@@ -9,13 +9,13 @@ func Info(msg string) {
 	Infox(msg, nil)
 }
 
-// Infof logs a general informational message.
+// Info logs a general informational message.
 // Same as [Info] but provides a message formatting API.
 func Infof(format string, values ...any) {
 	Infox(fmt.Sprintf(format, values...), nil)
 }
 
-// Infox logs a general informational message.
+// Info logs a general informational message.
 // Same as [Info] but includes structured context.
 func Infox(msg string, extras map[string]any) {
 	for _, l := range loggers {
