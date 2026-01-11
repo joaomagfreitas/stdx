af98d42c63595876cee0e2b650d2f67a209a02d4
João Freitas <1160907@isep.ipp.pt>
2026-01-11 23:08:12 +0000

    [errorsx] - add structured error

---
+++ errorsx/structured.go
@@ -0,0 +1,49 @@
+package errorsx
+
+import (
+	"fmt"
+	"strings"
+)
+
+// StructuredError represents an error with structured context.
+// It can wrap an underlying error while adding origin, category,
+// operation, and a human-readable message.
+type StructuredError struct {
+	Origin    string
+	Category  string
+	Operation string
+	Message   string
+	Wrapped   error
+}
+
+func (err StructuredError) Error() string {
+	var sb strings.Builder
+	args := []any{}
+
+	if len(err.Origin) > 0 {
+		sb.WriteString("origin: %s, ")
+		args = append(args, err.Origin)
+	}
+
+	if len(err.Category) > 0 {
+		sb.WriteString("category: %s, ")
+		args = append(args, err.Category)
+	}
+
+	if len(err.Operation) > 0 {
+		sb.WriteString("operation: %s, ")
+		args = append(args, err.Operation)
+	}
+
+	if len(err.Message) > 0 {
+		sb.WriteString("message: %s, ")
+		args = append(args, err.Message)
+	}
+
+	if err.Wrapped != nil {
+		sb.WriteString("err: %v")
+		args = append(args, err.Wrapped)
+	}
+
+	return fmt.Sprintf(strings.TrimSuffix(sb.String(), ", "), args...)
+}
