b9c4985691ca57aaf4bfd1de8c3bdcf86b7e6050
João Freitas <joao.mag.freitas@gmail.com>
2026-01-26 20:49:01 +0000

    [requestx] - fix multiple read of form values (#28)

--- httpx/requestx/form.go
+++ httpx/requestx/form.go
@@ -3,6 +3,6 @@ package requestx
 import "net/http"
 
 // Form extracts the value of the request form associated to [key].
-func Form(r http.Request, key string) string {
+func Form(r *http.Request, key string) string {
 	return r.FormValue(key)
 }
