9bcde6f7313d4637e88ab05a318bc1e588e6ab51
João Freitas <joao.mag.freitas@gmail.com>
2026-04-04 16:15:10 +0100

    [sqlx] - add fluent api to create insert queries

---
+++ sqlx/builder/insert.go
@@ -0,0 +1,124 @@
+package sqlx_builder
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/joaomagfreitas/stdx/slicesx"
+)
+
+type insertBuilder struct {
+	table              string
+	columns            []string
+	values             []string
+	returning          []string
+	placeholderMapping PlaceholderMapping
+}
+
+// PlaceholderMapping defines a function that maps a parameter index
+// (starting at 0) to a SQL placeholder string.
+//
+// It can be used to adapt the builder to different SQL dialects.
+//
+// Example:
+//
+//	// PostgreSQL-style placeholders: $1, $2, ...
+//	mapping := func(i int) string {
+//	    return fmt.Sprintf("$%d", i+1)
+//	}
+type PlaceholderMapping func(index int) string
+
+// Insert allows to create an INSERT SQL query in a fluent manner,
+// by providing a builder API for each supported clause.
+// //
+// Example:
+//
+//	query := Insert().
+//	    Into("users").
+//	    Columns("name", "age").
+//	    String()
+//
+//	// Output:
+//	// INSERT INTO users (name, age)
+//	// VALUES (?, ?);
+func Insert() *insertBuilder {
+	return &insertBuilder{
+		placeholderMapping: func(index int) string { return "?" },
+	}
+}
+
+// Into sets the target table name for the INSERT statement. The table name is treated "as-is".
+func (b *insertBuilder) Into(table string) *insertBuilder {
+	b.table = table
+	return b
+}
+
+// Columns sets the column names for the INSERT statement. The column names are treated "as-is".
+func (b *insertBuilder) Columns(columns ...string) *insertBuilder {
+	b.columns = columns
+	return b
+}
+
+// Values sets explicit values for the INSERT statement.
+//
+// If not provided, placeholders will be generated automatically
+// based on the number of columns and the placeholder mapping function.
+func (b *insertBuilder) Values(values ...string) *insertBuilder {
+	b.values = values
+	return b
+}
+
+// PlaceholderMapping overrides the default placeholder generation logic.
+//
+// Useful for supporting different SQL dialects (e.g. PostgreSQL).
+//
+// Example:
+//
+//	query := Insert().
+//	    Into("users").
+//	    Columns("name", "age").
+//	    PlaceholderMapping(func(i int) string {
+//	        return fmt.Sprintf("$%d", i+1)
+//	    }).
+//	    String()
+//
+//	// Output:
+//	// INSERT INTO users (name, age)
+//	// VALUES ($1, $2);
+func (b *insertBuilder) PlaceholderMapping(placeholderMapping PlaceholderMapping) *insertBuilder {
+	b.placeholderMapping = placeholderMapping
+	return b
+}
+
+// Returning sets the column names to return inserted values for the INSERT statement.
+// The column names are treated "as-is".
+func (b *insertBuilder) Returning(returning ...string) *insertBuilder {
+	b.returning = returning
+	return b
+}
+
+// String builds and returns the final SQL INSERT query as a string.
+func (b *insertBuilder) String() string {
+	var sb strings.Builder
+	fmt.Fprintf(&sb, "INSERT INTO %s", b.table)
+
+	if len(b.columns) > 0 {
+		fmt.Fprintf(&sb, " (%s)", strings.Join(b.columns, ", "))
+	}
+
+	var vs []string
+	if len(b.values) > 0 {
+		vs = b.values
+	} else {
+		vs = slicesx.Gen(len(b.columns), b.placeholderMapping)
+	}
+	fmt.Fprintf(&sb, "\nVALUES (%s)", strings.Join(vs, ", "))
+
+	if len(b.returning) > 0 {
+		fmt.Fprintf(&sb, "\nRETURNING (%s)", strings.Join(b.returning, ", "))
+	}
+
+	sb.WriteRune(';')
+
+	return sb.String()
+}
---
+++ sqlx/builder/insert_test.go
@@ -0,0 +1,99 @@
+package sqlx_builder_test
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"testing"
+
+	sqlx_builder "github.com/joaomagfreitas/stdx/sqlx/builder"
+)
+
+func TestInsert(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		sql     string
+		builder func() string
+	}{
+		{
+			desc: "joins values following the order they were passed",
+			builder: func() string {
+				return sqlx_builder.
+					Insert().
+					Into("foo").
+					Values("1", "2", "3").
+					String()
+			},
+			sql: `
+			INSERT INTO foo
+			VALUES (1, 2, 3);`,
+		},
+		{
+			desc: "joins placeholder parameters for each column, if values are not passed",
+			builder: func() string {
+				return sqlx_builder.
+					Insert().
+					Into("foo").
+					Columns("bar", "lorem", "ipsum").
+					String()
+			},
+			sql: `
+			INSERT INTO foo (bar, lorem, ipsum)
+			VALUES (?, ?, ?);`,
+		},
+		{
+			desc: "uses placeholder mapping function to map parameters for each column, if values are not passed",
+			builder: func() string {
+				return sqlx_builder.
+					Insert().
+					Into("foo").
+					Columns("bar", "lorem", "ipsum").
+					PlaceholderMapping(func(index int) string { return fmt.Sprintf("$%d", index+1) }).
+					String()
+			},
+			sql: `
+			INSERT INTO foo (bar, lorem, ipsum)
+			VALUES ($1, $2, $3);`,
+		},
+		{
+			desc: "does not join placeholder parameters for each column, if values are passed",
+			builder: func() string {
+				return sqlx_builder.
+					Insert().
+					Into("foo").
+					Columns("bar", "lorem", "ipsum").
+					Values("1", "2", "3").
+					String()
+			},
+			sql: `
+			INSERT INTO foo (bar, lorem, ipsum)
+			VALUES (1, 2, 3);`,
+		},
+		{
+			desc: "includes passed column names in returning statement",
+			builder: func() string {
+				return sqlx_builder.
+					Insert().
+					Into("foo").
+					Columns("bar", "lorem", "ipsum").
+					Values("1", "2", "3").
+					Returning("lorem", "ipsum").
+					String()
+			},
+			sql: `
+			INSERT INTO foo (bar, lorem, ipsum)
+			VALUES (1, 2, 3)
+			RETURNING (lorem, ipsum);`,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			tkb := strings.Fields(tC.builder())
+			tke := strings.Fields(tC.sql)
+
+			if !slices.Equal(tkb, tke) {
+				t.Fatalf("\nexp: %v\ngot: %v", tke, tkb)
+			}
+		})
+	}
+}
