e9e94a145b00ed2990a384f4988c07d2f515aa21
João Freitas <joao.mag.freitas@gmail.com>
2026-04-20 19:01:30 +0100

    [routerx] - add middleware func that transforms an error handler func in a normal handler func (#39)

--- httpx/routerx/handler.go
+++ httpx/routerx/handler.go
@@ -4,3 +4,6 @@ import "net/http"
 
 // An [http.HandlerFunc] that returns an error.
 type ErrorHandlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+// A middleware func that accepts an [ErrorHandlerFunc] in return for a [http.HandlerFunc].
+type ErrorHandler func(handlerFunc ErrorHandlerFunc) http.HandlerFunc
