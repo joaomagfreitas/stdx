c597739841b88c7a6361ba790f6c014329ec9701
João Freitas <joao.mag.freitas@gmail.com>
2026-04-20 17:15:17 +0100

    [routerx] - doc typo

--- httpx/routerx/handler.go
+++ httpx/routerx/handler.go
@@ -2,5 +2,5 @@ package routerx
 
 import "net/http"
 
-// Alias for [http.HandlerFunc] that returns an error.
+// An [http.HandlerFunc] that returns an error.
 type ErrorHandlerFunc func(w http.ResponseWriter, r *http.Request) error
