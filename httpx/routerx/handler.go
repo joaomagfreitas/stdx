9e4394aa6b0464139efa01ba4563213826c18716
João Freitas <joao.mag.freitas@gmail.com>
2026-04-20 17:14:52 +0100

    [routerx] - treat error handler func as new type

--- httpx/routerx/handler.go
+++ httpx/routerx/handler.go
@@ -3,4 +3,4 @@ package routerx
 import "net/http"
 
 // Alias for [http.HandlerFunc] that returns an error.
-type ErrorHandlerFunc = func(w http.ResponseWriter, r *http.Request) error
+type ErrorHandlerFunc func(w http.ResponseWriter, r *http.Request) error
